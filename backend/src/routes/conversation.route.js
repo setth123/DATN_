199cd5353e63fe77a7182fe88929ce5847c0d921
Edward Laurent <thanhtkcb2004@gmail.com>
2026-08-11 17:13:02 +0700

    Delete backend directory

--- backend/src/routes/conversation.route.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import * as conversationController from "../controllers/conversation.controller.js";
-import { authMiddleware } from "../middlewares/auth.middeware.js";
-
-const router=express.Router();
-
-router.post("/",authMiddleware,conversationController.getOrCreateConversation);
-router.post("/ai",authMiddleware,conversationController.createOrGetAIConversation);
-export default router;
